61a637ef760af7c19b88c9f603ab546fd661d3a5
Maung945 <myoaung@cpp.edu>
2022-10-21 12:33:07 -0700

    First Commit

---
+++ numberguessing.c
@@ -0,0 +1,11 @@
+/*
+*   Myo Aung- Number Guessing Game- CS-2600 System Programming
+*   Frirst set Global variable as 10 and declare variables.
+*   Prompt user to Choose options 1-3
+*   Do-while loop to repeate till Optioni-3 is entered.
+*   If User entered enry other than "Numbers" promopt user to enter a new number
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
